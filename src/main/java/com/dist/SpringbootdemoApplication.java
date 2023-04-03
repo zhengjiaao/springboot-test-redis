c39d23c0fe48d9c25824f793697e24aee18d7576
Zhengjiaao <zhengja@dist.com.cn>
2023-04-03 11:19:41 +0800

    Remove 移除不用的文件

--- src/main/java/com/dist/SpringbootdemoApplication.java
+++ src/main/java/com/dist/SpringbootdemoApplication.java
@@ -3,6 +3,9 @@ package com.dist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * swagger2：<a href="http://localhost:8080/springboot-test-redis/swagger-ui.html">...</a>
+ */
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
